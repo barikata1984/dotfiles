d30e4493c328785019058a29fee5f05097a10ac6
barikata1984 <franche1984@gmail.com>
2024-10-14 15:35:00 -0400

    start using starship first at powershells

--- initiate-windows.ps1
+++ initiate-windows.ps1
@@ -15,6 +15,10 @@ winget install --id Nvidia.GeForceExperience --source winget
 winget install --id wez.wezterm --source winget
 # Sonos controller
 winget install --id Sonos.Controller --source winget
+# vswhere
+winget install --id Microsoft.VisualStudio.Locator --source winget
+# LLVM
+winget install --id LLVM.LLVM --source winget
 # FiraCode
 & ([scriptblock]::Create((iwr 'https://to.loredo.me/Install-NerdFont.ps1'))) -Name fira-code
 
